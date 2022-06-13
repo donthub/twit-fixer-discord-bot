28e1785c7448dc20c03e8a9ebd93a48cef5bbc9e
Beni B <dontsmail@gmail.com>
2022-06-13 22:58:12 +0200

    Added configurable per channel restrictions.

--- README.md
+++ README.md
@@ -2,19 +2,29 @@
 
 ## Description
 
-If twitter.com link is posted without other content, deletes the message and re-posts it as vxtwitter.com link, mentioning the original poster.
+If **twitter.com** link is posted without other content, deletes the message and re-posts it as **vxtwitter.com** link, mentioning the original poster.
 
 ## Set-up
 
-* Copy `config.sample.json` to `config.json` and provide the bot's token.
-* Bot must have the following:
+* Create bot with following scopes and permissions:
   * Scopes
     * `bot`
   * Permissions
-    * Read Messages/View Channels
-    * Send Messages
-    * Send Messages in Threads
-    * Manage Messages
+    * `Read Messages/View Channels`
+    * `Send Messages`
+    * `Send Messages in Threads`
+    * `Manage Messages`
+* Generate OAuth link and add the bot to the server(s)
+
+## Configuration
+
+* Copy `config.sample.json` to `config.json` and provide:
+  * `token`: The bot's token
+  * `servers`: 
+    * `id`: Server ID
+    * `channels`: Channel IDs
+      * `include`: List of channels to replace messages in. If empty, any non-excluded channels of the server will apply.
+      * `exclude`: List of channels to not replace messages in.
 
 ## Usage
 
--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-// Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token, servers } = require('./config.json');
 
-// Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
-    console.log('Ready!');
+    console.log('Application started');
 });
 
-// When new message is posted
 client.on('messageCreate', message => {
     const re = /^\s*https:\/\/([^\s]+\.)?twitter.com\/([^\s?]+)(\?[^\s]*)?\s*$/;
     const match = re.exec(message.content);
-    if (match) {
-        message.delete();
-        message.channel.send(`Originally posted by <@${message.author.id}>\nhttps://vxtwitter.com/${match[2]}`);
+    if (!match || isRestricted(message)) {
+        return;
     }
+
+    console.log(`Recreating message in ${message.guild.name} / ${message.channel.name}`);
+    message.delete();
+    message.channel.send(`Originally posted by <@${message.author.id}>\nhttps://vxtwitter.com/${match[2]}`);
 });
 
-// Login to Discord with your client's token
 client.login(token);
+
+function isRestricted(message) {
+    const serverId = message.guild.id;
+    const included = [];
+    const excluded = [];
+    for (const server of servers) {
+        if (server.id !== serverId) {
+            continue;
+        }
+        included.push(...server.channels.included);
+        excluded.push(...server.channels.excluded);
+    }
+
+    const channelId = message.channel.id;
+    return excluded.includes(channelId) || included.length && !included.includes(channelId);
+}
